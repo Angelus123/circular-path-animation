fa25510c3aa4579b7a415c95a9e40d13882ca3f1
Angelus123 <izerefaifelix@gmail.com>
2022-04-13 12:25:14 +0200

    change the business logic by move all image at the same time

--- index.js
+++ index.js
@@ -87,207 +87,141 @@ var listItemsSixth = sixthAlbum.map((item) => {
 })
 sixthAlbumInner.innerHTML = listItemsSixth.join(" ")
 
-const albumOneHandle = () => {
-  let popedOne = firstAlbum.shift()
+const firstAlbumHandle = () => {
+  let shiftedFirst = firstAlbum.shift()
+  let shiftedSecond = firstAlbum.shift()
+  let shiftedThird = firstAlbum.shift()
+  let shiftedFourth = firstAlbum.shift()
+  let shiftedFifth = firstAlbum.shift()
+  let shiftedSixth = firstAlbum.shift()
+  listItemsOne = firstAlbum.map((item) => {
+    return `${item}`
+  })
+  firstAlbumInner.innerHTML = listItemsOne.join(" ")
   let itemNum = firstAlbum.length
-  let itemToAnimate=`${popedOne.substring(0, popedOne.length-2)}`
-  let animateId= null
-  let image =null
-  switch (itemNum) {
-    case 5:
-      fourthAlbum.push(itemToAnimate +`id="animate-fourth" />`)
-      animateId ="animate-fourth"
-      listItemsOne = firstAlbum.map((item) => {
-        return `${item}`
-      })
-      firstAlbumInner.innerHTML = listItemsOne.join(" ")
-      listItemsFourth = fourthAlbum.map((item) => {
-        return `${item}`
-      })
-      fourthAlbumInner.innerHTML = listItemsFourth.join(" ")
+  console.log(itemNum)
+  let firstItemToAnimate = `${shiftedFirst.substring(0, shiftedFirst.length - 2)}`
+  let animateId = null
+  fourthAlbum.push(firstItemToAnimate + `id="animate-fourth" />`)
+  animateId = "animate-fourth"
+  listItemsOne = firstAlbum.map((item) => {
+    return `${item}`
+  })
+  firstAlbumInner.innerHTML = listItemsOne.join(" ")
+  listItemsFourth = fourthAlbum.map((item) => {
+    return `${item}`
+  })
+  fourthAlbumInner.innerHTML = listItemsFourth.join(" ")
+  let secondItemToAnimate = `${shiftedSecond.substring(0, shiftedSecond.length - 2)}`
+  fithAlbum.push(secondItemToAnimate + `id="animate-fith" />`)
+  animateId = "animate-fith"
+  listItemsFith = fithAlbum.map((item) => {
+    return `${item}`
+  })
+  fithAlbumInner.innerHTML = listItemsFith.join(" ")
 
-      break;
+  let thirdItemToAnimate = `${shiftedThird.substring(0, shiftedThird.length - 2)}`
+  sixthAlbum.push(thirdItemToAnimate + `id="animate-sixth" />`)
+  animateId = "animate-sixth"
+  listItemsOne = firstAlbum.map((item) => {
+    return `${item}`
+  })
+  listItemsSixth = sixthAlbum.map((item) => {
+    return `${item}`
+  })
+  sixthAlbumInner.innerHTML = listItemsSixth.join(" ")
 
-    case 4:
-      fithAlbum.push(itemToAnimate+`id="animate-fith" />`)
-      animateId ="animate-fith"
-      listItemsOne = firstAlbum.map((item) => {
-        return `${item}`
-      })
-      firstAlbumInner.innerHTML = listItemsOne.join(" ")
-      listItemsFith = fithAlbum.map((item) => {
-        return `${item}`
-      })
-      fithAlbumInner.innerHTML = listItemsFith.join(" ")
+  let fourthItemToAnimate = `${shiftedFourth.substring(0, shiftedFourth.length - 2)}`
+  thirdAlbum.push(fourthItemToAnimate + `id="animate-third" />`)
+  animateId = "animate-third"
+  listItemsOne = firstAlbum.map((item) => {
+    return `${item}`
+  })
+  firstAlbumInner.innerHTML = listItemsOne.join(" ")
+  listItemsThird = thirdAlbum.map((item) => {
+    return `${item}`
+  })
+  thirdAlbumInner.innerHTML = listItemsThird.join(" ")
+  let fifthItemToAnimate = `${shiftedFifth.substring(0, shiftedFifth.length - 2)}`
+  secondAlbum.push(fifthItemToAnimate + `id="animate-second" />`)
+  animateId = "animate-second"
+  listItemsOne = firstAlbum.map((item) => {
+    return `${item}`
+  })
+  listItemsTwo = secondAlbum.map((item) => {
+    return `${item}`
+  })
+  secondAlbumInner.innerHTML = listItemsTwo.join(" ")
 
-      break;
+  let sixthItemToAnimate = `${shiftedSixth.substring(0, shiftedSixth.length - 2)}`
 
-    case 3:
-      sixthAlbum.push(itemToAnimate+`id="animate-sixth" />`)
-      animateId ="animate-sixth"
-      listItemsOne = firstAlbum.map((item) => {
-        return `${item}`
-      }) 
-      firstAlbumInner.innerHTML = listItemsOne.join(" ")
-      listItemsSixth = sixthAlbum.map((item) => {
-        return `${item}`
-      })
-      sixthAlbumInner.innerHTML = listItemsSixth.join(" ")
-
-      break;
-    case 2:
-      thirdAlbum.push(itemToAnimate+`id="animate-third" />`)
-      animateId ="animate-third"
-      listItemsOne = firstAlbum.map((item) => {
-        return `${item}`
-      })
-      firstAlbumInner.innerHTML = listItemsOne.join(" ")
-      listItemsThird = thirdAlbum.map((item) => {
-        return `${item}`
-      })
-      thirdAlbumInner.innerHTML = listItemsThird.join(" ")
-
-
-      break;
-    case 1:
-      secondAlbum.push(itemToAnimate+`id="animate-second" />`)
-      animateId ="animate-second"
-      listItemsOne = firstAlbum.map((item) => {
-        return `${item}`
-      })
-      firstAlbumInner.innerHTML = listItemsOne.join(" ")
-      listItemsTwo = secondAlbum.map((item) => {
-        return `${item}`
-      })
-      secondAlbumInner.innerHTML = listItemsTwo.join(" ")
-
-
-      break;
-
-    default:
-      alert("Remain only one picture")
-      break;
-
-      
-  }
-  myMove(animateId)
+  firstAlbum.push(sixthItemToAnimate + `id="animate-second" />`)
+  animateId = "animate-third"
+  listItemsOne = firstAlbum.map((item) => {
+    return `${item}`
+  })
+  let listItemsfirst = firstAlbum.map((item) => {
+    return `${item}`
+  })
+  firstAlbumInner.innerHTML = listItemsfirst.join(" ")
+  // myMove(animateId)
 
 }
 
-const albumSecondHandle = () => {
-  let poped = secondAlbum.pop()
+const albumOtherHandle = () => {
+  let popedFirst = firstAlbum.pop()
+  let popedSecond = secondAlbum.pop()
+  let popedThird = thirdAlbum.pop()
+  let popedFourth = fourthAlbum.pop()
+  let popedFifth = fithAlbum.pop()
+  let popedSixth = sixthAlbum.pop()
   let itemNum = secondAlbum.length
   if (itemNum >= 6) {
-    firstAlbum.unshift(poped)
+    firstAlbum.unshift(popedFirst)
+    firstAlbum.unshift(popedSecond)
+    firstAlbum.unshift(popedThird)
+    firstAlbum.unshift(popedFourth)
+    firstAlbum.unshift(popedFifth)
+    firstAlbum.unshift(popedSixth)
     listItemsSecond = secondAlbum.map((item) => {
       return `${item}`
     })
     secondAlbumInner.innerHTML = listItemsSecond.join(" ")
+
     listItemsOne = firstAlbum.map((item) => {
       return `${item}`
     })
     firstAlbumInner.innerHTML = listItemsOne.join(" ")
-  }
-  else {
-    alert("Not enough")
-  }
 
-
-
-}
-
-const albumThirdHandle = () => {
-  let poped = thirdAlbum.pop()
-  let itemNum = thirdAlbum.length
-  if (itemNum >= 7) {
-    firstAlbum.unshift(poped)
     listItemsThird = thirdAlbum.map((item) => {
       return `${item}`
     })
     thirdAlbumInner.innerHTML = listItemsThird.join(" ")
-    listItemsOne = firstAlbum.map((item) => {
-      return `${item}`
-    })
-    firstAlbumInner.innerHTML = listItemsOne.join(" ")
-  }
-  else {
-    alert("Not enough")
-  }
 
-
-
-}
-
-const albumFourthHandle = () => {
-  let poped = fourthAlbum.pop()
-  let itemNum = fourthAlbum.length
-  if (itemNum >= 7) {
-    firstAlbum.unshift(poped)
     listItemsFourth = fourthAlbum.map((item) => {
       return `${item}`
     })
     fourthAlbumInner.innerHTML = listItemsFourth.join(" ")
-    listItemsOne = firstAlbum.map((item ) => {
+
+    listItemsFifth = fithAlbum.map((item) => {
       return `${item}`
     })
-    firstAlbumInner.innerHTML = listItemsOne.join(" ")
-  }
-  else {
-    alert("Not enough")
-  }
+    fithAlbumInner.innerHTML = listItemsFifth.join(" ")
 
-
-
-}
-
-const albumFithHandle = () => {
-  let poped = fithAlbum.pop()
-  let itemNum = fithAlbum.length
-  if (itemNum >= 6) {
-    firstAlbum.unshift(poped)
-    listItemsFith = fithAlbum.map((item) => {
-      return `${item}`
-    })
-    fithAlbumInner.innerHTML = listItemsFith.join(" ")
-    listItemsOne = firstAlbum.map((item) => {
-      return `${item}`
-    })
-    firstAlbumInner.innerHTML = listItemsOne.join(" ")
-  }
-  else {
-    alert("Not enough")
-  }
-
-
-
-}
-
-const albumSixthHandle = () => {
-  let poped = sixthAlbum.pop()
-  let itemNum = sixthAlbum.length
-  if (itemNum >= 7) {
-    firstAlbum.unshift(poped)
     listItemsSixth = sixthAlbum.map((item) => {
       return `${item}`
     })
     sixthAlbumInner.innerHTML = listItemsSixth.join(" ")
-    listItemsOne = firstAlbum.map((item) => {
-      return `${item}`
-    })
-    firstAlbumInner.innerHTML = listItemsOne.join(" ")
+
   }
   else {
     alert("Not enough")
   }
-
-
-
 }
-
 
 function myMove(animate) {
   var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
-  window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+    window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
   // var field = document.getElementById("field");
   var ball = document.getElementById(animate);
 
@@ -300,12 +234,11 @@ function myMove(animate) {
   var start = null;
   var stretchFactor;
 
-  function step(timestamp)
-  {
+  function step(timestamp) {
     var progress, x, y;
-    if(start === null) {
+    if (start === null) {
       start = timestamp;
-      
+
       stretchFactor = 1 + (Math.random() * 3);
     }
     progress = (timestamp - start) / duration / 1000; // percent
@@ -313,10 +246,10 @@ function myMove(animate) {
     x = stretchFactor * Math.sin(progress * 2 * Math.PI); // x = ƒ(t)
     y = Math.cos(progress * 2 * Math.PI); // y = ƒ(t)
 
-    ball.style.left = maxX/2 + (gridSize * x) + "px";
-    ball.style.bottom = maxY/2 + (gridSize * y) + "px";
+    ball.style.left = maxX / 2 + (gridSize * x) + "px";
+    ball.style.bottom = maxY / 2 + (gridSize * y) + "px";
 
-    if(progress >= 1) start = null; // reset to start position
+    if (progress >= 1) start = null; // reset to start position
     requestAnimationFrame(step);
   }
 
